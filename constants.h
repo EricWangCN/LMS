c6dfaf1897c6929e9f975a1da9eee782f0d7e6e9
EricWangCN <wangzilong2016@outlook.com>
2020-07-01 22:33:41 +0800

    加入中文注释

--- constants.h
+++ constants.h
@@ -1,35 +1,19 @@
-/*
- * Copyright (c) 2008-2011 Zhang Ming (M. Zhang), zmjerry@163.com
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 2 or any later version.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details. A copy of the GNU General Public License is available at:
- * http://www.fsf.org/licensing/licenses
- */
+/*******************************************************************
+ *          基于西安交通大学 M.Zhang 的常量Header File
+ *          北京交通大学 王子龙 wangzilong@bjtu.edu.cn
+ *          用于小学期综合训练大作业
+ *******************************************************************/
 
 
-/*****************************************************************************
- *                                 constants.h
+/*******************************************************************
+ *                          constants.h
  *
- * Some constants often used in numeric computing.
+ * 一些用于数值计算的常量
  *
- * Zhang Ming, 2010-01, Xi'an Jiaotong University.
- *****************************************************************************/
+ * 代码来源：Zhang Ming, zmjerry@163.com, 编写与2010年01月
+ * 代码修改：王子龙, wangzilong@bjtu.edu.cn, CS1804, SCIT, BJTU
+ * 代码修改日期：2020年07月01日
+ ********************************************************************/
 
 
 #ifndef	CONSTANTS_H
